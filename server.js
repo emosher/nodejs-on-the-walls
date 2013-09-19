705a922e4337e6993c63724cf11f0d9bfa0bb99a
Eric Mosher <emoshmosh@gmail.com>
2013-09-18 21:18:07 -0400

    Adding options to keep file open and append to the file.

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 var net = require('net'),
     fs = require('fs');
 
-var file = fs.createWriteStream(__dirname + '/logs.json'),
+var file = fs.createWriteStream(__dirname + '/logs.json', { flags: 'a' }),
     server = net.createServer();
 
 server.on('connection', function(socket) {
-    socket.pipe(file);
+    socket.pipe(file, { end: false });
 });
 
 server.listen(4000, function() {
