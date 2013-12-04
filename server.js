8ff2f02c582cdfbcf06fe9f3134d6b45799ec843
Eric Mosher <emoshmosh@gmail.com>
2013-12-03 20:27:26 -0500

    Incomplete progress on new branch. Wiping machine.

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
  * Dependencies.
  */
 var net = require('net'),
-    fs = require('fs');
+    fs = require('fs'),
+    JSONStream = require('json-stream');
 
 var file = fs.createWriteStream(__dirname + '/logs.json', { flags: 'a' }),
     server = net.createServer();
 
 server.on('connection', function(socket) {
-    socket.pipe(file, { end: false });
+    var jsonStream = new JSONStream();
+    socket.pipe(jsonStream).pipe(file, { end: false });
 });
 
 server.listen(4000, function() {
